9d12c4ef9032bec7dee9a4334fc6a7fab6b87233
Gula612 <gula612612@gmail.com>
2020-04-20 18:11:44 +0300

    Новый файл Coffee.js

---
+++ script.js
@@ -0,0 +1,51 @@
+"use strict"
+/* let coffeeMachine = document.querySelector(".coffee-machine");
+ coffeeMachine.style.border = "10px solid darkblue";
+ coffeeMachine.style.borderRadius = "25px";
+ coffeeMachine.style.position = "absolute";
+ coffeeMachine.style.top = "15px";
+ coffeeMachine.style.left = "150px";*/
+ //let coffeeMachineTop = coffeeMachine.style.top;
+ //console.log(parseInt(coffeeMachineTop));
+ 
+ 
+/* let cupImages = document.querySelectorAll(".coffee-item img, .coffee-cup img");
+ console.log(cupImages);
+ cupImages.addEventListenerAll("click", function() {
+   alert("rrr");
+ });
+*/ 
+/* let cupImage = document.querySelector(".coffee-cup img");
+ console.log(cupImage);
+ cupImage.addEventListener("click", function() {
+   alert('scr="img/americano.png"');
+ });
+
+*/
+/* let clickButton = document.querySelector("button");
+ 
+ //let clientH = document.body.clientHeight;
+ //подлинная высота окна браузера - это сложно пока, но кнопка ниже работает! :)))
+ clickButton.addEventListener("click", function() {
+   alert('Высота окна браузера равна N');
+ });
+*/ 
+ 
+/* <body> <button onclick="alert(document.documentElement.clientHeight)">Кнопка</button> <script> //Здесь ничего нет </script> </body>
+*/
+   
+ 
+ 
+ 
+ 
+/* 
+ setTimeout(paintBody, 5000);
+ function paintBody() {
+   document.body.style.background = "crimson";
+ }
+*/ 
+ 
+ 
+ 
+ 
+ 
